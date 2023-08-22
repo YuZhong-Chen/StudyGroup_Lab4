791415db73be619023c1c85f1aee9cbd9c760e55
YuZhong-Chen <0980027@gmail.com>
2023-08-23 04:08:52 +0800

    Init.

---
+++ .devcontainer/post_install.sh
@@ -0,0 +1,6 @@
+# Disable showwing (bash) in terminal
+conda config --set changeps1 False
+
+# Install git lfs
+# curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+# sudo apt install -y git-lfs
